d7ef1e9a68fc1987410d850685609e94c4c29906
rkadlick <27776470+rkadlick@users.noreply.github.com>
2024-04-21 19:43:23 -0400

    near-final update

--- js/server.js
+++ js/server.js
@@ -1,38 +1,33 @@
+// Import necessary modules
 require('dotenv').config();
-
-const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const emailUser = process.env.EMAIL_USER;
-const emailPassword = process.env.EMAIL_PASSWORD;
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Serve static files (if needed)
-app.use(express.static('public'));
-
 // Simple contact form route
 app.post('/contact', (req, res) => {
+  console.log('TEST')
   const { name, email, message } = req.body;
 
-  // Replace with your email configuration
+  // Email configuration
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'your_email@gmail.com',
-      pass: 'your_email_password',
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: 'your_email@gmail.com',
-    to: 'your_email@gmail.com',
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
     subject: 'New Contact Form Submission',
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
   };
@@ -44,6 +39,9 @@ app.post('/contact', (req, res) => {
     res.status(200).send('Message sent: ' + info.response);
   });
 });
+
+// Serve static files (if needed)
+app.use(express.static('public'));
 
 // Start the server
 app.listen(port, () => {
